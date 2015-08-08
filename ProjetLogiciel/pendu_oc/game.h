c1ddccd6111da0c268f222c929ed41283fb7c4cd
cernkor <cernkor@localhost.localdomain>
2015-08-08 17:07:38 +0200

    Classe game

--- ProjetLogiciel/pendu_oc/game.h
+++ ProjetLogiciel/pendu_oc/game.h
@@ -1,10 +1,16 @@
 #ifndef GAME_H
 #define GAME_H
+#include "dictionnary.h"
 
 class game
 {
 public:
     game();
+private:
+    int errorsCount;
+    char selectedLetter;
+    char usedLetters[26];
+    dictionnary dico;
 };
 
 #endif // GAME_H
