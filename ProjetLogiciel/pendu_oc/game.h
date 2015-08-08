53bdd060e421fa8056a6baf3892026184158e8b2
cernkor <cernkor@localhost.localdomain>
2015-08-08 17:45:13 +0200

    Code

--- ProjetLogiciel/pendu_oc/game.h
+++ ProjetLogiciel/pendu_oc/game.h
@@ -1,15 +1,28 @@
 #ifndef GAME_H
 #define GAME_H
 #include "dictionnary.h"
+#include <QChar>
 
 class game
 {
 public:
     game();
+    int getErrorsCount() const;
+    void setErrorsCount(int value);
+
+    QChar getSelectedLetter() const;
+    void setSelectedLetter(QChar value);
+
+    int getUsedLetters(int pos) const;
+    void setUsedLetters(int value, int pos);
+
+    bool compareLetterToWord(QChar selectedLetter);
+    bool compareLettersToUsedLetters(QChar selectedLetter);
+
 private:
     int errorsCount;
-    char selectedLetter;
-    char usedLetters[26];
+    QChar selectedLetter;
+    int usedLetters[26];
     dictionnary dico;
 };
 
